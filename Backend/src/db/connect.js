7a25eed2fcbb05913c45ee4e7760f3079e4e06f0
Papun Pal <papunpal38029@gmail.com>
2026-05-06 10:27:13 +0530

    fixed  db connection

--- Backend/src/db/connect.js
+++ Backend/src/db/connect.js
@@ -7,7 +7,15 @@ const connectDb = async () => {
         if (!process.env.MONGO_URI || !DB_NAME) {
             throw new Error("MONGO_URI or DB_NAME is not defined in the environment variables.");
         }
-        await mongoose.connect(`${process.env.MONGO_URI}${DB_NAME}`);
+        await mongoose.connect(`${process.env.MONGO_URI}${DB_NAME}`,{
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+            connectTimeoutMS: 10000,
+            retryWrites: true,
+            retryReads: true,
+            maxPoolSize: 10,
+            minPoolSize: 2,
+        });
         console.log("MongoDB connected !!");
     } catch (error) {
         console.log("MONGOOSE connect error..",error);
